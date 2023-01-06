4c55ed1a025432d9adb64819397a10c1c39a5961
NavodBW <navodbw@gmail.com>
2023-01-06 14:04:55 +0800

    only allows a single aphabetical letter as input

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
-An improved version of my original hangman game (https://github.com/NavodBW/hangman-cpp); where instead of asking the user for the secret word, the game radomly selects a word that is longer than 3 letters from a text file. (text file obtained from: https://github.com/dwyl/english-words)
-Uses mersenne_twister_engine (https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine) to genarate a random number and select a word from the text.
+An improved version of my original hangman game (https://github.com/NavodBW/hangman-cpp)
+
+IMPROVEMENTS:
+-Instead of asking the user for the secret word, the game radomly selects a word that is longer than 3 letters from a text file. (text file obtained from: https://github.com/dwyl/english-words)
+-Uses mersenne_twister_engine (https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine) to genarate a random number and select a word from the text.
+-Implemented input validation where the game converts the input into lowercase, only allows single alphabetical characters.
+-Ignores the words with numbers or special chars in the text file 
 
--- hangmanv2.cpp
+++ hangmanv2.cpp
@@ -79,68 +79,68 @@ int main(){
 
     // Print the number of remaining incorrect guesses
     std::cout << "Remaining Incorrect Guesses: " << MAX_INCORRECT_GUESSES - numIncorrectGuesses << std::endl;
-    // Prompt the player to enter a guess
-    std::cout << "Enter a guess: ";
-    char guess;
-    std::cin >> guess;
 
-    // Convert the guess to lowercase
-    guess = tolower(guess);
+      // Prompt the player to enter a guess
+      std::cout << "\nEnter a guess: ";
+      std::string guess;
+      std::getline(std::cin, guess);
 
-    // Validate that the guess is an alphabetical letter
-    if (!isalpha(guess))
-    {
-      std::cout << "Error: Please enter an alphabetical letter." << std::endl;
-      continue;
-    }
-
-    // Check if the guess is correct
-    if (std::find(secretWord.begin(), secretWord.end(), guess) != secretWord.end())
-    {
-      // Add the guess to the correct guesses vector
-      correctGuesses.push_back(guess);
-
-      // Check if the player has won
-      bool won = true;
-      for (char ch : secretWord)
+      // Validate that the guess is a single alphabetical letter
+      if (guess.length() != 1 || !isalpha(guess[0]))
       {
-        if (std::find(correctGuesses.begin(), correctGuesses.end(), ch) == correctGuesses.end())
+        std::cout << "Error: Please enter a single alphabetical letter." << std::endl;
+        continue;
+      }
+
+      // Convert the guess to lowercase
+      guess[0] = tolower(guess[0]);
+
+      // Check if the guess is correct
+      if (std::find(secretWord.begin(), secretWord.end(), guess[0]) != secretWord.end())
+      {
+        // Add the guess to the correct guesses vector
+        correctGuesses.push_back(guess[0]);
+
+        // Check if the player has won
+        bool won = true;
+        for (char ch : secretWord)
         {
-          won = false;
+          if (std::find(correctGuesses.begin(), correctGuesses.end(), ch) == correctGuesses.end())
+          {
+            won = false;
+            break;
+          }
+        }
+
+        if (won)
+        {
+          std::cout << "Congratulations, you won!" << std::endl;
           break;
         }
       }
-
-      if (won)
+      else
       {
-        std::cout << "Congratulations, you won!" << std::endl;
-        break;
+        // Add the guess to the incorrect guesses vector
+        incorrectGuesses.push_back(guess[0]);
+        numIncorrectGuesses++;
       }
     }
-    else
+
+    if (numIncorrectGuesses == MAX_INCORRECT_GUESSES)
     {
-      // Add the guess to the incorrect guesses vector
-      incorrectGuesses.push_back(guess);
-      numIncorrectGuesses++;
+      std::cout << "You lost :( The secret word was: " << secretWord << std::endl;
+    }
+
+    // Ask the player if they want to play again
+    std::cout << "Do you want to play again (y/n)? ";
+    char playAgain;
+    std::cin >> playAgain;
+
+    if (playAgain == 'n')
+    {
+      break;
     }
   }
-
-  if (numIncorrectGuesses == MAX_INCORRECT_GUESSES)
-  {
-    std::cout << "You lost :( The secret word was: " << secretWord << std::endl;
-  }
-
-  // Ask the player if they want to start a new game or quit
-  std::cout << "Play again? (y/n) ";
-  char playAgain;
-  std::cin >> playAgain;
-  if (playAgain == 'n')
-  {
-    break;
-  }
-  }
-
-system("pause");
-return 0;
-
+  system("pause");
+  return 0;
 }
